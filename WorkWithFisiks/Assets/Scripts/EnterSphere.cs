28fbee6a60d0585ade826bd733023f721f040a96
Sh-Revo <shandov@i.ua>
2022-06-01 20:56:03 +0300

    Add fisiks

---
+++ WorkWithFisiks/Assets/Scripts/EnterSphere.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+using UnityEngine.UI;
+
+public class EnterSphere : MonoBehaviour
+{
+    [SerializeField] private float _time;
+    [SerializeField] private Text _timerText;
+    private static bool _flag = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        EnterText();
+    }
+
+    private float _timeLeft = 0f;
+    private void EnterText()
+    {
+        _timeLeft = _time;
+        StartCoroutine(StartTimer());
+    }
+
+    private IEnumerator StartTimer()
+    {
+        while (_timeLeft > 0)
+        {
+            _timeLeft -= Time.deltaTime;
+            UpdateTimeText();
+            yield return null;
+        }
+    }
+    private void UpdateTimeText()
+    {
+        if (_timeLeft < 0)
+        {
+            flag = true;
+            _timeLeft = 0;
+        }
+
+        //float minutes = Mathf.FloorToInt(_timeLeft / 60);
+        float seconds = Mathf.FloorToInt(_timeLeft % 60);
+        _timerText.text = string.Format("{00}", seconds);
+    }
+
+    public static bool flag { get => _flag; set => _flag = value; }
+}
---
+++ WorkWithFisiks/Assets/Scripts/Explosion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+    [SerializeField] private float _force;
+
+    private void Update()
+    {
+        if (EnterSphere.flag == true)
+        {
+            Explode();
+        }
+        
+    }
+
+    public void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _radius);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody rigidbody = colliders[i].attachedRigidbody;
+            if (rigidbody != null)
+            {
+                rigidbody.AddExplosionForce(_force, transform.position, _radius);
+            }
+        }
+        
+    }
+}
